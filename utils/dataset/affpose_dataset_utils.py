062c74cf7fdc303bc519a843d8ffd995da5cc7a3
akeaveny <akeaveny@uwaterloo.ca>
2021-04-09 09:25:35 -0400

    Syn Scripts

--- utils/dataset/affpose_dataset_utils.py
+++ utils/dataset/affpose_dataset_utils.py
@@ -19,13 +19,14 @@ def convert_obj_part_mask_to_obj_mask(obj_part_mask):
     obj_mask = np.zeros((obj_part_mask.shape[0], obj_part_mask.shape[1]), dtype=np.uint8)
 
     obj_part_ids = np.unique(obj_part_mask)[1:]
+    # obj_part_ids = np.flip(obj_part_ids)
     for obj_part_id in obj_part_ids:
         obj_id = map_obj_part_id_to_obj_id(obj_part_id)
-        # print(f'obj_part_id:{obj_part_id}, obj_id:{obj_id}')
-        obj_mask_one = np.ones((obj_part_mask.shape[0], obj_part_mask.shape[1]), dtype=np.uint8)
-        obj_mask_one = obj_mask_one * obj_id
-        obj_mask = np.where(obj_part_mask==obj_part_id, obj_mask_one, obj_mask).astype(np.uint8)
-    # helper_utils.print_class_labels(obj_mask)
+        # print(f'obj_part_id:{obj_part_id}, obj_id:{aff_id}')
+        aff_mask_one = np.ones((obj_part_mask.shape[0], obj_part_mask.shape[1]), dtype=np.uint8)
+        aff_mask_one = aff_mask_one * obj_id
+        obj_mask = np.where(obj_part_mask==obj_part_id, aff_mask_one, obj_mask).astype(np.uint8)
+    # helper_utils.print_class_labels(aff_mask)
     return obj_mask
 
 def convert_obj_part_mask_to_aff_mask(obj_part_mask):
